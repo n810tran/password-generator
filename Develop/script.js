b65b740e31dd81e750aaf3980a43ca8ae117e420
n810tran <nathaniel.k.tran@gmail.com>
2023-08-24 09:59:31 -0700

    reorganized files

--- Develop/script.js
+++ Develop/script.js
@@ -1,3 +1,14 @@
+/*
+when button is pressed, prompt user for password criteria
+when prompted, allow user to select criteria
+choose a length from 8 - 128 characters
+confirm inclusion of lowercase,uppercase,numeric, and/or special characters
+when answering each prompt, input should be validated and at least one character type should be selected
+when all prompts answered, then password matching criteria is generated
+
+*/
+
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
