8e912909168ba8017b8c8869985c880ecb1b497e
n810tran <nathaniel.k.tran@gmail.com>
2023-08-24 18:51:13 -0700

    added possible characters

--- script.js
+++ script.js
@@ -1,43 +1,35 @@
-/*
-
-
-
-
-
-
-
-*/
-
+// when button is pressed, prompt user for password criteria
+// choose a length from 8 - 128 characters
+// when answering each prompt, input should be validated and at least one character type should be selected
+// confirm inclusion of lowercase,uppercase,numeric, and/or special characters
+// when all prompts answered, then password matching criteria is generated
 
 // Assignment Code
 const generateBtn = document.querySelector("#generate");
-// when button is pressed, prompt user for password criteria
+
+// Define possibnle characters
+var specialCharacters = ['!' ,'"' ,'#' ,'$' ,'%' ,'&' ,"'" ,'(' ,')' ,'*' ,'+' ,',' ,'-' ,'.' ,'/' ,':' ,';' ,'<' ,'=' ,'>' ,'?' ,'@' ,'[' ,'\\' ,']' ,'^' ,'_' ,'`' ,'{' ,'|' ,'}' ,'~'];
+var numericCharacters = ['0' ,'1' ,'2' ,'3' ,'4' ,'5' ,'6' ,'7' ,'8' ,'9']
+var uppercaseCharacters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+var lowercaseCharacters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+function generatePassword() {
+  passwordLength = prompt("How many characters in you password? Choose between 8 and 128 characters.");
+  if (passwordLength < 8) {
+    alert("Please enter a length of at lease 8 characters.")
+
+  }
+}
+
+
+
+
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-
-
-// choose a length from 8 - 128 characters
-
-
-
-// when answering each prompt, input should be validated and at least one character type should be selected
-
-
-
-// confirm inclusion of lowercase,uppercase,numeric, and/or special characters
-
-
-// when all prompts answered, then password matching criteria is generated
+  const password = generatePassword();
+  const passwordText = document.querySelector("#password");
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-function passwordlength() {
-  const userInput = prompt
-}
