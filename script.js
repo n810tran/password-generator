159534e12a7f4b5485c9357435dcf1507a54b027
n810tran <nathaniel.k.tran@gmail.com>
2023-08-24 23:05:42 -0700

    added comments, fixed formatting

--- script.js
+++ script.js
@@ -1,26 +1,27 @@
 // when button is pressed, prompt user for password criteria
 // choose a length from 8 - 128 characters
-// when answering each prompt, input should be validated and at least one character type should be selected
 // confirm inclusion of lowercase,uppercase,numeric, and/or special characters
+// when answering each prompt, input should be validated and at least one character type should be selected
 // when all prompts answered, then password matching criteria is generated
 
 // Assignment Code
+
 const generateBtn = document.querySelector("#generate");
 
 // Define possible characters in categories
 var lowercaseCharacters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 var uppercaseCharacters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
-var numericCharacters = ['0' ,'1' ,'2' ,'3' ,'4' ,'5' ,'6' ,'7' ,'8' ,'9']
-var specialCharacters = ['!' ,'"' ,'#' ,'$' ,'%' ,'&' ,"'" ,'(' ,')' ,'*' ,'+' ,',' ,'-' ,'.' ,'/' ,':' ,';' ,'<' ,'=' ,'>' ,'?' ,'@' ,'[' ,'\\' ,']' ,'^' ,'_' ,'`' ,'{' ,'|' ,'}' ,'~'];
+var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+var specialCharacters = ['!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', '`', '{', '|', '}', '~'];
 var characterPool = []
 
 //Generate password with user input
 function generatePassword() {
   passwordLength = prompt("How many characters would you like in you password? Choose between 8 and 128 characters.");
-//need a way to stop function if no correct number value is chosen
+  //Need a way to stop function if no correct number value is chosen
   if (passwordLength < 8) {
     alert("Please enter a length of at least 8 characters.");
-return "Try Again"
+    return "Try Again" 
   } else if (passwordLength > 128) {
     alert("Please enter a length up to 128 characters.");
     return "Try Again"
@@ -61,6 +62,7 @@ return "Try Again"
   }
   console.log(hasSpecial)
 
+  //check to make sure at least 1 character type is selected
   if (hasLowercase === false && hasUppercase === false && hasNumeric === false && hasSpecial === false) {
     alert("Please select at least one character type.");
     return "Try Again"
@@ -84,21 +86,15 @@ return "Try Again"
     console.log(characterPool)
   }
 
-  //check to make sure at least 1 choi
-
   //generate random password from characterPool
-  let randomizedPassword ="";
+  let randomizedPassword = "";
   for (let i = 0; i < passwordLength; i++) {
-        let randomSelection = (Math.floor(Math.random() * characterPool.length));
+    let randomSelection = (Math.floor(Math.random() * characterPool.length));
     console.log(characterPool[randomSelection]);
     randomizedPassword = randomizedPassword + characterPool[randomSelection];
   }
   return randomizedPassword;
 }
-
-
-
-
 
 // Write password to the #password input
 function writePassword() {
